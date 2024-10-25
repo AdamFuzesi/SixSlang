4fce6067e4cbdec1f78aac81c94c19810b665452
AdamFuzesi <adamfuzeri@gmail.com>
2024-10-25 12:43:28 -0300

    Initial Commit

---
+++ CrodieTranslator/CrodieTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrodieTranslatorApp.swift
+//  CrodieTranslator
+//
+//  Created by Adam Fuzesi on 2024-10-25.
+//
+
+import SwiftUI
+
+@main
+struct CrodieTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrodieTranslatorTests/CrodieTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrodieTranslatorTests.swift
+//  CrodieTranslatorTests
+//
+//  Created by Adam Fuzesi on 2024-10-25.
+//
+
+import Testing
+@testable import CrodieTranslator
+
+struct CrodieTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
